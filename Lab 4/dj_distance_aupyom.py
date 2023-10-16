43aca82a6bef49a034635c27394504ff4198c33c
HaukeCornell <hgs52@cornell.edu>
2023-10-16 11:53:50 -0400

    edited several files

--- Lab 4/dj_distance_aupyom.py
+++ Lab 4/dj_distance_aupyom.py
@@ -1,14 +1,93 @@
+#!/usr/bin/env python
+#-----------------------------------------------------------------------------
+# qwiic_proximity_ex1.py
+#
+# Simple Example for the Qwiic Proximity Device
+#------------------------------------------------------------------------
+#
+# Written by  SparkFun Electronics, May 2019
+# 
+# This python library supports the SparkFun Electroncis qwiic 
+# qwiic sensor/board ecosystem on a Raspberry Pi (and compatable) single
+# board computers. 
+#
+# More information on qwiic is at https://www.sparkfun.com/qwiic
+#
+# Do you like this library? Help support SparkFun. Buy a board!
+#
+#==================================================================================
+# Copyright (c) 2019 SparkFun Electronics
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy 
+# of this software and associated documentation files (the "Software"), to deal 
+# in the Software without restriction, including without limitation the rights 
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+# copies of the Software, and to permit persons to whom the Software is 
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all 
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+# SOFTWARE.
+#==================================================================================
+# Example 1
+#
+# - Setup the device
+# - Output the proximity value
+from __future__ import print_function
+import qwiic_proximity
+import time
+import sys
 from aupyom import Sampler, Sound
 from aupyom.util import example_audio_file
-import time
 
-sampler = Sampler()
+def runExample():
 
-audio_file = "music/Time2Shine.wav"
-s1 = Sound.from_file(audio_file)
+    #load sound
+    sampler = Sampler()
+    s1 = Sound.from_file("music/Time2Shine.wav")
+    sampler.play(s1)
 
-sampler.play(s1)
+    #start sensoring
+	print("\nSparkFun Proximity Sensor VCN4040 Example 1\n")
+	oProx = qwiic_proximity.QwiicProximity()
 
+	if oProx.connected == False:
+		print("The Qwiic Proximity device isn't connected to the system. Please check your connection", \
+			file=sys.stderr)
+		return
+
+	oProx.begin()
+
+	while True:
+		proxValue = oProx.get_proximity()
+		print("Proximity Value: %d" % proxValue)
+		
+		if proxValue <= 6000:
+			s1.pitch_shift = +10
+		elif proxValue <= 12000:
+			s1.pitch_shift = 0
+		else:
+			s1.pitch_shift = -10
+		time.sleep(.2)
+
+
+if __name__ == '__main__':
+	try:
+		runExample()
+	except (KeyboardInterrupt, SystemExit) as exErr:
+		print("\nEnding Example 1")
+		sys.exit(0)
+
+
+
+"""
 time.sleep(5)
 s1.pitch_shift = 5
 print('pitch shifted')
@@ -20,4 +99,4 @@ print('pitch shifted')
 time.sleep(5)
 s1.pitch_shift = -8
 print('pitch shifted')
-
+"""
