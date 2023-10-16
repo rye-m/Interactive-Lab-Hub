e064b6ae5f2119b7d74db56332a8e11e2c1047c3
HaukeCornell <hgs52@cornell.edu>
2023-10-15 22:22:57 -0400

    Confirmed to work correctly 	modified:   Lab 4/dj_distance_aupyom.py 	modified:   Lab 4/test.ipynb

--- Lab 4/dj_distance_aupyom.py
+++ Lab 4/dj_distance_aupyom.py
@@ -1,10 +1,23 @@
 from aupyom import Sampler, Sound
 from aupyom.util import example_audio_file
-import numpy
+import time
 
 sampler = Sampler()
 
-audio_file = example_audio_file()
+audio_file = "music/Time2Shine.wav"
 s1 = Sound.from_file(audio_file)
 
 sampler.play(s1)
+
+time.sleep(5)
+s1.pitch_shift = 5
+print('pitch shifted')
+
+time.sleep(5)
+s1.pitch_shift = 15
+print('pitch shifted')
+
+time.sleep(5)
+s1.pitch_shift = -8
+print('pitch shifted')
+
